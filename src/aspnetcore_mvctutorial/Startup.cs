c38a27f81e5a30eea5980e319b83524bddd59119
iotjack <iotjack@outlook.com>
2016-06-12 22:12:06 -0700

    seed localdb

--- src/aspnetcore_mvctutorial/Startup.cs
+++ src/aspnetcore_mvctutorial/Startup.cs
@@ -83,6 +83,9 @@ namespace aspnetcore_mvctutorial
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
+
+            //SeedData.Initialize(app.ApplicationServices);
+
         }
     }
 }
